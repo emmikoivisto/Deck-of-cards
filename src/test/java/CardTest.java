75099cbe54c5c151f26256d5c0e40fb9cdd57d76
emmikoivisto <emetsi@gmail.com>
2020-11-19 14:11:44 +0000

    card test initialised

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,13 @@
+import org.junit.Before;
+
 public class CardTest {
+
+    Card playingCard;
+
+    @Before
+    public void before(){
+        playingCard = new Card(SuitType.CLUBS, RankType.ACE);
+    }
+
+
 }
